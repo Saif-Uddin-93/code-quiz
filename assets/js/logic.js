12fb78df55700df20825c28a178a4d3267921574
Saif Uddin <admin@saifuddin.uk>
2023-12-14 15:22:53 +0000

    changed timer to a function

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,8 +4,30 @@ const questionsElement = htmlElement('#questions');
 const feedbackEl = htmlElement('#feedback');
 const endScreenElement = htmlElement('#end-screen');
 //const correctSound = ;
-let timer_bool=false;
-let deductTime = (time) => timeElement.textContent=parseInt(timeElement.textContent)-time;
+//let timer_bool=false;
+//const startTime = 100;
+//timeElement.textContent = startTime;
+const deductTime = (time) => timeElement.textContent=parseInt(timeElement.textContent)-time;
+const currentTime = () => parseInt(timeElement.textContent);
+const maxQuestions = Object.entries(questions).length;
+
+const Timer = (bool = timeElement.dataset.active, time = currentTime()) => ({active: String(timeElement.dataset.active), setActive : timeElement.dataset.active = bool.toString(), setTime : timeElement.textContent = time/* || currentTime() */})
+
+Timer(false, 100);//.setActive;
+
+function startTimer() {
+    console.log(Timer().active)
+    if(Timer().active==="true"){
+        //timeElement.textContent = parseInt(timeElement.textContent)-1;
+        deductTime(1);
+        if(currentTime()<1){
+            outOfTime();
+        }
+    } else {
+        setInterval(startTimer, 1000);
+        Timer(true, 100);
+    }
+}
 
 function buildQuestion(questionNo){
     const questionTitleElement = htmlElement('#question-title');
@@ -17,17 +39,16 @@ function buildQuestion(questionNo){
 }
 
 function nextQuestion(eventObj){
-    const maxQuestions = Object.entries(questions).length;
     if(eventObj.target.id==="start") {
-        timer_bool = true;
+        //timer_bool = true;
+        //Timer(true)//.setActive;
+        startTimer();
+        console.log(`timer bool: ${Timer().active}`);
         eventObj.target.parentNode.classList.toggle('hide');
         questionsElement.classList.toggle('hide');
     }
     if(questionsElement.dataset.nextQuestion==maxQuestions+1) {
-        timer_bool=false;
-        clearInterval(startTimer);
-        endScreenElement.classList.toggle("hide")
-        questionsElement.classList.toggle("hide")
+        endScreen();
         return;
     }
     
@@ -36,17 +57,20 @@ function nextQuestion(eventObj){
     questionsElement.setAttribute("data-next-question", currentQ+1);
 }
 
+
 function checkAnswer(eventObj) {
     if (eventObj.target.dataset.correct=='false'){
         console.log(eventObj.target.dataset.correct);
-        timeElement.textContent=parseInt(timeElement.textContent)-20;
+        //timeElement.textContent=parseInt(timeElement.textContent)-20;
+        deductTime(20);
         feedbackEl.textContent = 'Wrong!'
         feedbackEl.classList.remove('hide');
         //feedbackToggle();
         setTimeout(toggleHide, 1000);
-        /* if(){
-
-        } */
+        if(currentTime()<1){
+            console.log(`current time is: ${currentTime()}`)
+            outOfTime();
+        }
     }
     else {
         console.log(eventObj.target.dataset.correct);
@@ -65,32 +89,26 @@ function scoreSubmit(eventObj) {
     location.href = "highscores.html";
 }
 
-function startTimer() {
-    if(timer_bool){
-        timeElement.textContent = parseInt(timeElement.textContent)-1;
-        if(parseInt(timeElement.textContent)<1){
-            outOfTime();
-        }
-    }
+function outOfTime(){
+    endScreen();
+    timeElement.textContent = 0;
+    console.log(`Out of time!`);
 }
 
-function outOfTime(){
-    timer_bool = false;
+function endScreen() {
+    Timer(false);
     clearInterval(startTimer);
-    timeElement.textContent = 0;
-    endScreenElement.classList.toggle("hide")
-    questionsElement.classList.toggle("hide")
+    endScreenElement.classList.remove("hide");
+    questionsElement.classList.add("hide");
 }
 
 function toggleHide() {
     feedbackEl.classList.add('hide');
 }
 
-setInterval(startTimer, 1000);
-
-addGlobalEventListener('click', NextQuestion, '#start');
-addGlobalEventListener('click', CheckAnswer, '.choice');
-addGlobalEventListener('click', ScoreSubmit, '#submit');
+addGlobalEventListener('click', nextQuestion, '#start');
+addGlobalEventListener('click', checkAnswer, '.choice');
+addGlobalEventListener('click', scoreSubmit, '#submit');
 
 function addGlobalEventListener(typeOfEvent, callback, selector, stopPropagation=false) {
     document.addEventListener(typeOfEvent, (eventObj) => {
