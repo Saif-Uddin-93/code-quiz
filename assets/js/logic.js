cd1eaeb9f1ff86892922220d7ac12a8bbc0da3c6
Saif Uddin <admin@saifuddin.uk>
2023-12-14 17:27:50 +0000

    stored countdown interval as a variable

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -10,18 +10,19 @@ const endScreenElement = htmlElement('#end-screen');
 const maxQuestions = Object.entries(questions).length;
 const deductTime = (time) => timeElement.textContent=parseInt(timeElement.textContent)-time;
 const currentTime = () => parseInt(timeElement.textContent);
-
+let timeInterval;
 const Timer = () => ({
     setActive : (bool = timeElement.dataset.active)=>timeElement.dataset.active = bool.toString(), 
+    active : ()=> String(timeElement.dataset.active),
     setTime : (time = currentTime())=>timeElement.textContent = time,
-    start : ()=>{setInterval(countdown, 1000); Timer().setActive(true)},
-    stop : ()=>{clearInterval(countdown); Timer().setActive(false)},
-    active : String(timeElement.dataset.active)
+    start : ()=>{ timeInterval = setInterval(countdown, 1000); Timer().setActive(true)},
+    stop : ()=>{clearInterval(timeInterval); Timer().setActive(false)},
 })
 
 function countdown() {
     deductTime(1);
     if(currentTime()<1){
+        console.log(`countdown() called`);
         outOfTime();
     }
 }
@@ -44,7 +45,7 @@ function nextQuestion(eventObj){
         //Timer(true)//.setActive;
         //startTimer();
         Timer().start();
-        console.log(`timer bool: ${Timer().active}`);
+        console.log(`timer bool: ${Timer().active()}`);
         eventObj.target.parentNode.classList.toggle('hide');
         questionsElement.classList.toggle('hide');
     }
@@ -70,6 +71,7 @@ function checkAnswer(eventObj) {
         setTimeout(toggleHide, 1000);
         if(currentTime()<1){
             console.log(`current time is: ${currentTime()}`)
+            console.log(`checkAnswer() called`);
             outOfTime();
         }
     }
@@ -99,7 +101,6 @@ function outOfTime(){
 function endScreen() {
     Timer().stop();
     //clearInterval(startTimer);
-    //Timer().stop
     endScreenElement.classList.remove("hide");
     questionsElement.classList.add("hide");
 }
